25ddeecf6ae518e909bff7cdbf0246222f3ebafe
unknown <abuhasanxt@gmail.com>
2026-09-10 00:18:59 +0600

    delete unnecessary OTP via cron

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { indexRoutes } from "./routes";
 import { notFound } from "./middleware/notFound";
 import { errorHandler } from "./middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
+import cron from "node-cron"
+import { prisma } from "./lib/prisma";
 const app: Application = express();
 
 // Enable URL-encoded form data parsing
@@ -12,6 +14,22 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser())
+//  Expired OTP cleanup 
+cron.schedule("* * * * *", async () => {
+  try {
+    const result = await prisma.emailVerification.deleteMany({
+      where: {
+        expiresAt: {
+          lt: new Date(),
+        },
+      },
+    });
+
+    console.log(`Expired OTP deleted: ${result.count}`);
+  } catch (error) {
+    console.error("OTP cleanup failed:", error);
+  }
+});
 app.use("/",indexRoutes)
 // Basic route
 app.get('/', (req: Request, res: Response) => {
