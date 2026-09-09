704b26c89474b31432a4b8d151b002c8789a333a
unknown <abuhasanxt@gmail.com>
2026-09-09 19:50:13 +0600

    create accessToken, refreshToken,jwt,setCookie

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,24 +4,40 @@ import status from "http-status";
 
 dotenv.config();
 interface EnvConfig {
-  NODE:string;
+  NODE: string;
   PORT: string;
   DATABASE_URL: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES_IN: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_REFRESH_EXPIRES_IN: string;
 }
 
 const loadEnvVariables = () => {
-  const requiredEnvVariable = ["NODE","PORT", "DATABASE_URL"];
+  const requiredEnvVariable = [
+    "NODE",
+    "PORT",
+    "DATABASE_URL",
+    "JWT_ACCESS_SECRET",
+    "JWT_REFRESH_SECRET",
+    "JWT_REFRESH_EXPIRES_IN",
+  ];
   requiredEnvVariable.forEach((variable) => {
     if (!process.env[variable]) {
-      throw new AppError(status.INTERNAL_SERVER_ERROR,
+      throw new AppError(
+        status.INTERNAL_SERVER_ERROR,
         `Environment variable ${variable} is required but not set in .env file`,
       );
     }
   });
   return {
-    NODE:process.env.NODE as string,
+    NODE: process.env.NODE as string,
     PORT: process.env.PORT as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN as string,
   };
 };
 
